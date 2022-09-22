6f3d477623bd793cad8bf0b31fc92d9b29f97fb5
Jyestha <jyestha47@gmail.com>
2022-09-22 10:57:16 +0700

    delete new

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
     $pass = "";
     $name = "gd4_a_0918";
     
-    $con = new mysqli_connect($host,$user,$pass,$name);
+    $con = mysqli_connect($host,$user,$pass,$name);
     
     if(mysqli_connect_errno()){
         echo "Failed to connect : " . mysqli_connect_error();
