c6b6317cac9b2af330161521b7b559ede0ad6695
Jyestha <jyestha47@gmail.com>
2022-09-26 23:35:43 +0700

    test db.php

--- db.php
+++ db.php
@@ -5,8 +5,4 @@
     $name = "gd_a_0918";
     
     $con = mysqli_connect($host,$user,$pass,$name);
-    
-    if(mysqli_connect_errno()){
-        echo "Failed to connect : " . mysqli_connect_error();
-    }
 ?>
